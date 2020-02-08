5dc14250fc562b5479095251229ec511bd8919e0
Max Collins <collinsmax1999@gmail.com>
2020-02-08 16:12:09 +0800

    Initial Commit

---
+++ NumNet2.py
@@ -0,0 +1,114 @@
+### Preamble ###################################################################
+#
+# Author            : Max Collins
+#
+# Github            : https://github.com/maxcollins1999
+#
+# Title             : NumNet2.py 
+#
+# Date Last Modified: 8/1/2020
+#
+# Notes             : A basic number classifier without the use of tensorflow
+#
+################################################################################
+
+### Imports ####################################################################
+
+#Local
+import phom
+
+#Global
+import pathlib
+import pickle
+
+################################################################################
+
+### Paths for Pathlib ##########################################################
+
+train_ims = pathlib.Path(__file__).parent / 'Num Images' / 'Train.gz'
+train_labels = pathlib.Path(__file__).parent / 'Num Images' / 'Train Labels.gz'
+test_ims = pathlib.Path(__file__).parent / 'Num Images' / 'Test.gz'
+test_labels = pathlib.Path(__file__).parent / 'Num Images' / 'Test Labels.gz'
+
+################################################################################
+
+class NumNet2:
+    
+    wmats = []    #List containing the numpy arrays of the edge weights
+    bmats = []    #List containign the numpy arrays of the b matrices
+    sig_param = 1 #Sigmoid function k parameter
+    train_images = [] #List of training images
+    train_labels = [] #List of corresponding training labels
+    
+    def __init__(self, noh=None, nih=None, nin=None, nou=None):
+        """Takes the number of hidden networks (noh), the number of nodes in 
+        hidden network (nih), number in (nin) and number out (nou), and 
+        constructs the initial b matrices.
+        """
+
+        
+        if noh and nih and nin and nou:
+            self.wmats.append(np.zeros((nih,nin)))
+            self.bmats.append(np.zeros((nih,1)))
+            for i in range(0,noh-1):
+                self.wmats.append(np.zeros((nih,nih)))
+                self.bmats.append(np.zeros((nih,1)))
+            self.wmats.append(np.zeros((nou,nih)))
+            self.bmats.append(np.zeros((nou,1)))
+        elif not (not noh and not nih and not nin and not nou):
+            raise AttributeError('Must assign complete network scaffold or no'+\
+                                 ' scaffold')
+        self.__readPhotos()
+
+
+### Public Methods #############################################################    
+    
+    def dispState(self):
+        """Displays the current values for each of the b matrices
+        """
+        for i, w in enumerate(self.wmats):
+            print('-----------------------')
+            print('b'+str(i)+'\n')
+            print(w)
+            print(w.shape)
+            print('-----------------------')
+            print(self.bmats[i])
+            print(self.bmats.shape)
+
+
+    def saveState(self):
+        """Saves the current object state as a pickle binary file
+        """
+
+        dump = {
+            'wmats':self.wmats,
+            'bmats':self.bmats
+        }
+        pickle.dump(dump,'save.txt')
+
+
+    def loadState(self)+:
+        """Loads the current object state from the pickle binary file
+        """
+
+        dump = pickle.load('save.txt')
+        self.wmats = dump['wmats']
+        self.bmats = dump['bmats']
+
+### Private Methods ############################################################
+        
+    def __readPhotos(self):
+        """Takes in the number of photos to be read and reads in the images 
+        that will be used to differentiate numbers and reshapes them into nx1 
+        arrays for use in matrix multiplication.
+        """
+        
+        self.train_images, self.train_labels = phom.getGzipped(n)
+        for im in self.train_images:
+            im.resize((len(im)*len(im[0]),1))
+
+### Tests ######################################################################
+ 
+
+
+################################################################################       
---
+++ phom.py
@@ -0,0 +1,105 @@
+from matplotlib import image as im
+from matplotlib import pyplot as plt
+import gzip
+import numpy as np
+import math
+
+
+idimx=28 #Max x dimension of image (pixels going down)
+idimy=28 #Max y dimension of image (pixels going across)
+
+def formpho(fname):
+    """Formats a raw photo for use in the neural network 
+    """
+    
+    rpic = im.imread(fname)
+    
+    x=len(rpic)
+    y=len(rpic[0])
+    
+    if len(rpic.shape)==3:
+        rpic = rgb2gray(rpic)
+        
+    if x > idimx and y > idimy:
+        rpic = rpic[:x-x%idimx,:y-y%idimy]
+        fpic = rpic[::x//idimx,::y//idimy]   
+        
+    elif x < idimx and y < idimy:
+        temp = np.zeros((idimx,idimy))
+        temp[:,:]=255
+        xpos = math.floor(0.5*(idimx-x))
+        ypos = math.floor(0.5*(idimy-y))
+        temp[xpos:xpos+x,ypos:ypos+y]=rpic
+        fpic=temp
+        
+    return fpic
+
+
+def rgb2gray(rgb):
+    """Converts an rgb photo to grayscale
+    """
+    
+    r = rgb[:,:,0]
+    g = rgb[:,:,1]
+    b = rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    
+    return gray
+
+
+def showGrayScale(pic):
+    """Prints the grayscale image to the terminal
+    """
+    
+    plt.imshow(pic,cmap='gray',vmin=0, vmax=255)
+    plt.show()
+    
+    
+def getGzipped(num_images=None):
+    """Strictly reads the gz files used int the number recognition neural 
+    network. These images are (28x28) pixels and are grayscale. Takes the 
+    number of images to read and returns a list of numpy arrays containing the 
+    images and a numpy array of the correct labels
+    """
+    
+    f = gzip.open('Train.gz','r')
+
+    image_size = 28
+    images = []
+    labels = []
+    
+    f.read(16)
+    if num_images is None:
+        buf = f.read()
+        num_images = int(len(buf)/(image_size * image_size))
+    else:
+        buf = f.read(image_size * image_size * num_images)
+    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    data = data.reshape(num_images, image_size, image_size, 1)
+    for val in data:
+        images.append(val.squeeze())    #REMINDER: Squeeze removes the unused 
+                                        #          3rd dimension
+    
+    
+    f = gzip.open('Train Labels.gz','r')
+    f.read(8)
+    for i in range(0,len(images)):   
+        buf = f.read(1)
+        labels.append(int.from_bytes(buf, "big"))
+        
+    return images, labels
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
